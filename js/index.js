a4702c3ede6a1b8df253327922ad60ec206ab5c1
sherzoddd <abduhakimovs274@gmail.com>
2021-12-01 19:07:43 +0500

    add footer

--- js/index.js
+++ js/index.js
@@ -4,3 +4,12 @@ const elHeader = document.querySelector(".header");
 elHamburgerBtn.addEventListener('click', ()=> {
   elHeader.classList.toggle('header--open');
 })
+
+
+
+// const elHamburgerBt = document.querySelector(".header__hamburger");
+// const elFooter = document.querySelector(".footer__top");
+
+// elHamburgerBt.addEventListener('click', ()=> {
+//   elFooter.classList.toggle('header--open');
+// })
